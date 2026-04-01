b2d02ff7e1b6c8c6a1c27698572fb676a6fe07f2
Sanjaylicet <sanjay.27csb@licet.ac.in>
2026-04-01 09:24:08 +0530

    error fix 1

--- demo/src/main/java/com/example/demo/controller/GreetingController.java
+++ demo/src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,18 +1,17 @@
-package com.example;
+package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class GreetingController {
 
-    // Access this at http://localhost:8080/greet
+    
     @GetMapping("/greet")
     public String sayHello() {
         return "Hello! Welcome to your Spring Boot application.";
     }
 
-    // Access this at http://localhost:8080/hello?name=User
+    
     @GetMapping("/hello")
     public String personalizedHello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello, %s!", name);
